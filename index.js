99f5def33489afa71af2eb1d873963b5e8d807d7
Tanjil Hassan <kenk14382@gmail.com>
2025-11-10 01:17:07 +0600

    import product delete system done

--- index.js
+++ index.js
@@ -150,6 +150,14 @@ async function run() {
 
     })
     
+    //DELETE for imported product
+    app.delete('/import/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      
+      const result = await importCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
